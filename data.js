a3738cb508f8034ac14db6756972871a1539f61e
Seung-Yoon Oh <feynubrick@gmail.com>
2018-12-22 11:22:49 +0900

    shopperlist function

--- data.js
+++ data.js
@@ -1,7 +1,9 @@
 var DATA = [new Dish('치즈라면', 4500), new Dish('김밥', 2000), new Dish('쫄면', 5000), new Dish('잔치국수', 5000), new Dish('우동', 5000), new Dish('돈까스', 7000)];
+var shooperList = [];
 
-function Dish(name, price, picture) {
+function Dish(name, price, picture, amount) {
     this.name = name;
     this.price = price;
     this.picture = picture;
+    this.amount = 0;
 }
--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // Script
+function refreshShopperList() {
+    var shopperListDiv = document.getElementById('shopper-list');
+
+    for (var dish of shooperList) {
+        var listItem = document.createElement('div');
+        shopperListDiv.appendChild(listItem);
+        var nameSpan = document.createElement('span');
+        var amountSpan = document.createElement('span');
+        var priceSpan = document.createElement('span');
+        shopperListDiv.appendChild(nameSpan);
+        shopperListDiv.appendChild(amountSpan);
+        shopperListDiv.appendChild(priceSpan);
+        
+
+        nameSpan.innerText(dish.name);
+        amountSpan.innerText(dish.amount);
+        priceSpan.innerText(String(dish.price * dish.amount) + '원');
+    }
+}
+
+function addShopperList(dish) {
+    dish.amount++;
+    addShopperList.push(dish);
+    refreshShopperList();
+}
