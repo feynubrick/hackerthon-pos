e00996680937eee7258a7a2d94f5698e63d5aa0d
Seung-Yoon Oh <feynubrick@gmail.com>
2018-12-23 19:58:46 +0900

    merged css design and completed basic functioning.

--- data.js
+++ data.js
@@ -1,9 +1,17 @@
-var DATA = [new Dish('치즈라면', 4500), new Dish('김밥', 2000), new Dish('쫄면', 5000), new Dish('잔치국수', 5000), new Dish('우동', 5000), new Dish('돈까스', 7000)];
+var DATA = [
+  new Dish("치즈라면", 4500),
+  new Dish("김밥", 2000),
+  new Dish("쫄면", 5000),
+  new Dish("잔치국수", 5000),
+  new Dish("우동", 5000),
+  new Dish("돈까스", 7000)
+];
+
 var shopperList = [];
 
 function Dish(name, price, picture, amount) {
-    this.name = name;
-    this.price = price;
-    this.picture = picture;
-    this.amount = 0;
+  this.name = name;
+  this.price = price;
+  this.picture = picture;
+  this.amount = 0;
 }
--- script.js
+++ script.js
@@ -3,49 +3,78 @@
 initial();
 
 function refreshShopperList() {
-    var shopperListDiv = document.getElementById('shopper-list');
+  var shopperListDiv = document.getElementById("shopper-list");
+  var shopperListTotalDiv = document.getElementById("shopper-list-total");
+  shopperListDiv.innerHTML =
+    '<div class="OrderMenu">메뉴</div><div class="OrderNumber">수량</div><div class="OrderPrice">금액</div>';
 
-    for (var dish of shopperList) {
-        var listItem = document.createElement('div');
-        shopperListDiv.appendChild(listItem);
-        var nameDiv = document.createElement('div');
-        var amountDiv = document.createElement('div');
-        var priceDiv = document.createElement('div');
-        shopperListDiv.appendChild(nameDiv);
-        shopperListDiv.appendChild(amountDiv);
-        shopperListDiv.appendChild(priceDiv);
+  for (var dish of shopperList) {
+    var listItem = document.createElement("div");
+    shopperListDiv.appendChild(listItem);
+    var nameDiv = document.createElement("div");
+    var amountDiv = document.createElement("div");
+    var priceDiv = document.createElement("div");
+    shopperListDiv.appendChild(nameDiv);
+    shopperListDiv.appendChild(amountDiv);
+    shopperListDiv.appendChild(priceDiv);
 
-        nameDiv.innerText = dish.name;
-        amountDiv.innerText = dish.amount;
-        priceDiv.innerText = String(dish.price * dish.amount) + '원';
-    }
+    nameDiv.innerText = dish.name;
+    amountDiv.innerText = dish.amount;
+    priceDiv.innerText = String(dish.price * dish.amount) + "원";
+
+    nameDiv.className = "OrderMenu";
+    amountDiv.className = "OrderNumber";
+    priceDiv.className = "OrderPrice";
+  }
+
+  var total = shopperList.reduce(function(total, item) {
+    return total + item.amount * item.price;
+  }, 0);
+  shopperListTotalDiv.innerHTML =
+    '<div id="shopper-list-total">합계</div> <div>' + total + " 원</div>";
 }
 
 function addShopperList(dish) {
-    dish.amount++;
+  var index = -1;
+  for (var i = 0; i < shopperList.length; i++) {
+    let item = shopperList[i];
+    if (dish.name === item.name) {
+      index = i;
+    }
+  }
+
+  if (index === -1) {
+    dish.amount = 1;
     shopperList.push(dish);
-    refreshShopperList();
+  } else {
+    shopperList[index].amount++;
+  }
+
+  // shopperList.push(dish);
+  refreshShopperList();
 }
 
 function initial() {
-    var menuList = document.getElementById("menu-list");
-    
-    for (let i = 0; i < DATA.length; i++) {
-        let dish = DATA[i];
+  var menuList = document.getElementById("menu-list");
 
-        var holder = document.createElement('div');
-        var menu = document.createElement("div");
-        var price = document.createElement("div");
+  for (let i = 0; i < DATA.length; i++) {
+    let dish = DATA[i];
 
-        menuList.appendChild(holder);
-        holder.appendChild(menu);
-        holder.appendChild(price);
+    let holder = document.createElement("div");
+    let menu = document.createElement("div");
+    let price = document.createElement("div");
 
-        holder.className = "menubox";
-        
-        menu.innerText = dish.name;
-        price.innerText = dish.price;
+    menuList.appendChild(holder);
+    holder.appendChild(menu);
+    holder.appendChild(price);
 
-        holder.onclick = function () { addShopperList(dish) };
-    }
+    holder.className = "menubox";
+
+    menu.innerText = dish.name;
+    price.innerText = dish.price;
+
+    holder.onclick = function() {
+      addShopperList(dish);
+    };
+  }
 }
