bda208ab8bbd178e0b939d2efdd20557249d0d8a
Seung-Yoon Oh <feynubrick@gmail.com>
2019-01-07 14:00:50 +0900

    css edited to use css-grid

--- script.js
+++ script.js
@@ -4,37 +4,28 @@ initial();
 
 function refreshShopperList() {
   var shopperListDiv = document.getElementById("shopper-list");
-  //   var shopperListTotalDiv = document.getElementById("shopper-list-total");
-  shopperListDiv.innerHTML =
-    '<div class="OrderMenu">메뉴</div><div class="OrderNumber">수량</div><div class="OrderPrice">금액</div>';
-
+  shopperListDiv.innerHTML = "";
+  var shopperListTotalDiv = document.getElementById("shopper-list-total-item");
   for (var dish of shopperList) {
     var listItem = document.createElement("div");
+    listItem.className = "shopper-list-item";
     shopperListDiv.appendChild(listItem);
     var nameDiv = document.createElement("div");
     var amountDiv = document.createElement("div");
     var priceDiv = document.createElement("div");
-    shopperListDiv.appendChild(nameDiv);
-    shopperListDiv.appendChild(amountDiv);
-    shopperListDiv.appendChild(priceDiv);
+    listItem.appendChild(nameDiv);
+    listItem.appendChild(amountDiv);
+    listItem.appendChild(priceDiv);
 
     nameDiv.innerText = dish.name;
     amountDiv.innerText = dish.amount;
     priceDiv.innerText = String(dish.price * dish.amount) + "원";
-
-    nameDiv.className = "OrderMenu";
-    amountDiv.className = "OrderNumber";
-    priceDiv.className = "OrderPrice";
   }
 
   var total = shopperList.reduce(function(total, item) {
     return total + item.amount * item.price;
   }, 0);
-  shopperListDiv.innerHTML =
-    shopperListDiv.innerHTML +
-    '<div id="shopper-list-total"><div>합계</div><div>' +
-    total +
-    " 원</div></div>";
+  shopperListTotalDiv.innerText = total + " 원";
 }
 
 function addShopperList(dish) {
@@ -53,7 +44,6 @@ function addShopperList(dish) {
     shopperList[index].amount++;
   }
 
-  // shopperList.push(dish);
   refreshShopperList();
 }
 
@@ -74,7 +64,7 @@ function initial() {
     holder.className = "menubox";
 
     menu.innerText = dish.name;
-    price.innerText = dish.price;
+    price.innerText = dish.price + " 원";
 
     holder.onclick = function() {
       addShopperList(dish);
